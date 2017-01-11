c744f19c6528b0b15df8e19680d27bba903ac0f2
lene marlin <ybs18s@163.com>
2017-01-11 19:14:04 +0800

    step6 order by wage

--- Gemfile
+++ Gemfile
@@ -41,9 +41,10 @@ group :development do
   gem 'web-console'
   gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  
+
 end
 gem "devise"
+gem 'font-awesome-rails'
 
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,10 +4,21 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false)
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
   end
 
 
